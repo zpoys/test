6c7db3af3d332872d1a733c5932cb98e7591effc
zpoys <haikal2589717@gmail.com>
2025-04-03 11:07:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get("/", async (req, res) => {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        "Referer": "https://v6.kuramanime.run/"
       },
     }
   );
